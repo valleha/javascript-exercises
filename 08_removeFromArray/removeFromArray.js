0f93c5a212f13c39e8e46506232fc559196ccec9
Valentin <hartervalentin@gmail.com>
2025-07-28 22:14:22 +0200

    Finish function to remove from an array
    The first test was no problem, got stuck on the others and had to look up ... operator

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+   return array.filter(val => !remove.includes(val));
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
