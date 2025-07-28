21ce398450ad9aaa94e96337f9e816ee371cccde
Valentin <hartervalentin@gmail.com>
2025-07-28 23:06:08 +0200

    Write function to sum all integers between a and b
    Changed the function a lot, as every new test presented new problem that made older code obsolete or to complicated. Finish the excercise wihtout help, though the solution is different.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+let sum = 0;
+if(Math.sign(a) == -1 || Math.sign(b) == -1)
+    {return "ERROR"}
 
-};
+else if(a < b && Number.isInteger(a) && Number.isInteger(b))
+    {for(let i = a; i <= b; i++){sum += i}
+}
+else if(a > b && Number.isInteger(a) && Number.isInteger(b)) 
+    {for(let i = b; i <= a; i++){sum += i}}
+
+else{return "ERROR"};
+
+return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
